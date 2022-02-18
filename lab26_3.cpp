42af602fe046f3a1972d3546ddf27b72d890b910
Panthaweekan <95071144+Panthaweekan@users.noreply.github.com>
2022-02-18 16:43:05 +0700

    FINISH lab26_3.cpp

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
-//Write function count() here.
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int count(int data[] , int size)
+{
+	set<int> numSet ;
+
+	for(int i = 0 ; i < size ; i++)
+	{
+
+		numSet.insert(data[i]) ;
+		
+	}
+	return numSet.size() ;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
